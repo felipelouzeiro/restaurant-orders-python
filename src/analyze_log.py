1080bcf0944fc1d011c98d4dac8a3009bc5b46be
felipe <fl.louzeiro@gmail.com>
2022-05-09 16:56:12 -0300

    feat: implementa analyzer_log e most_requested_dish_by_maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,36 @@
+import csv
+
+
+def most_requested_dish_by_maria(data):
+    foods = {}
+    for item in data:
+        if item["name"] == "maria":
+            if item["food"] not in foods:
+                foods[item["food"]] = 1
+            else:
+                foods[item["food"]] += 1
+
+    # https://docs.python.org/3/howto/sorting.html
+    most_requested_food = max(foods, key=lambda x: x[1])
+    print(data)
+    print("LISTAAAAAAAAAAAAAAAAA")
+
+    return most_requested_food
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if '.csv' in path_to_file:
+        try:
+            with open(path_to_file) as file:
+                data = list(csv.reader(file))
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+    else:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    most_requested_dish_by_maria_data = most_requested_dish_by_maria(data)
+
+    with open('data/mkt_campaign.txt') as file:
+        file.writelines([
+            f"{most_requested_dish_by_maria_data}\n",
+        ])
