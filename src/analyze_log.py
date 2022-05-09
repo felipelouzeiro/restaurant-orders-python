94fc06079bdb09f2472190d780e4651c894c1d9f
felipe <fl.louzeiro@gmail.com>
2022-05-09 19:33:24 -0300

    feat: implementa formatação com adicao de headers no arquivo de informações

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def most_requested_dish_by_maria(data):
+def most_requested_by_maria(data):
     foods = {}
     for item in data:
         if item["name"] == "maria":
@@ -11,24 +11,25 @@ def most_requested_dish_by_maria(data):
                 foods[item["food"]] += 1
 
     # https://docs.python.org/3/howto/sorting.html
-    most_requested_food = max(foods, key=lambda x: x[1])
+    most_requested_food = sorted(foods, key=lambda x: x[1])
 
-    return most_requested_food
+    return most_requested_food[0]
 
 
 def analyze_log(path_to_file):
     if '.csv' in path_to_file:
         try:
             with open(path_to_file) as file:
-                data = list(csv.reader(file))
+                header = ["name", "food", "day_of_the_week"]
+                data = csv.DictReader(file, fieldnames=header)
+                formatted_data = [line for line in data]
         except FileNotFoundError:
             raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
     else:
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
 
-    most_requested_dish_by_maria_data = most_requested_dish_by_maria(data)
-
-    with open('data/mkt_campaign.txt') as file:
+    most_requested_by_maria_data = most_requested_by_maria(formatted_data)
+    with open('data/mkt_campaign.txt', 'w') as file:
         file.writelines([
-            f"{most_requested_dish_by_maria_data}\n",
+            f"{most_requested_by_maria_data}\n",
         ])
