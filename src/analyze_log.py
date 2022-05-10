681b917044094982997b03609353976f36bc434f
felipe <fl.louzeiro@gmail.com>
2022-05-10 10:53:01 -0300

    refactor: padroniza header do arquivo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -4,11 +4,11 @@ import csv
 def most_requested_by_maria(data):
     foods = {}
     for item in data:
-        if item["name"] == "maria":
-            if item["food"] not in foods:
-                foods[item["food"]] = 1
+        if item["customer"] == "maria":
+            if item["order"] not in foods:
+                foods[item["order"]] = 1
             else:
-                foods[item["food"]] += 1
+                foods[item["order"]] += 1
 
     # https://docs.python.org/3/howto/sorting.html
     most_requested_food = sorted(foods, key=lambda x: x[1])
@@ -19,29 +19,29 @@ def most_requested_by_maria(data):
 def arnaldo_ordered(data):
     orders = 0
     for order in data:
-        if order["name"] == "arnaldo":
-            if order["food"] == "hamburguer":
+        if order["customer"] == "arnaldo":
+            if order["order"] == "hamburguer":
                 orders += 1
     return orders
 
 
 def unordered_dishes_by_joao(data):
-    menu = set(item["food"] for item in data)
+    menu = set(item["order"] for item in data)
 
     orders = set()
     for order in data:
-        if order["name"] == "joao":
-            orders.add(order["food"])
+        if order["customer"] == "joao":
+            orders.add(order["order"])
     unordered_dishes = menu.difference(orders)
     return unordered_dishes
 
 
 def days_not_frequented_by_joao(data):
-    days_of_week = set(item["day_of_the_week"] for item in data)
+    days_of_week = set(item["day"] for item in data)
     days = set()
     for order in data:
-        if order["name"] == "joao":
-            days.add(order["day_of_the_week"])
+        if order["customer"] == "joao":
+            days.add(order["day"])
     days_not_frequented = days_of_week.difference(days)
     return days_not_frequented
 
@@ -50,7 +50,7 @@ def analyze_log(path_to_file):
     if '.csv' in path_to_file:
         try:
             with open(path_to_file) as file:
-                header = ["name", "food", "day_of_the_week"]
+                header = ["customer", "order", "day"]
                 data = csv.DictReader(file, fieldnames=header)
                 formatted_data = [line for line in data]
         except FileNotFoundError:
